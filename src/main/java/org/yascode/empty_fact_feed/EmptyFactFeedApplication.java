14240fe2c43aa91e15f003b8724fa794b4af922f
Yassin MELLOUKI <yassin.mellouki.j@gmail.com>
2025-07-30 21:28:30 +0100

    first commit

---
+++ src/main/java/org/yascode/empty_fact_feed/EmptyFactFeedApplication.java
@@ -0,0 +1,13 @@
+package org.yascode.empty_fact_feed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmptyFactFeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmptyFactFeedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/yascode/empty_fact_feed/EmptyFactFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yascode.empty_fact_feed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmptyFactFeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
